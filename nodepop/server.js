f26100b01ae889e2d21d8d6d45181c5fa011880d
jonatahan-es <jonatahan_enriquez@hotmail.com>
2024-11-02 15:04:10 +0200

    Creacion de la extructuradel proyecto.Carpelas principales, archivos ,arrancamos el servidor

--- nodepop/server.js
+++ nodepop/server.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import http from 'node:http'
 
+// creamos una instancia de express
 const app = express()
 
-app.get('/',(req, res, next) => {
-    res.send('Hola jonathan')
+
+app.get('/', (req, res, next) => {
+    res.send('Hola')
 })
 
-
 // Create http server
-
 const server = http.createServer(app)
 
-server.on('listening', () => {console.log('Servicio arrancado')})
-server.listen(3001)
+server.on('listening',() => {console.log('Servidor arrancado')})
+server.listen(3100)
